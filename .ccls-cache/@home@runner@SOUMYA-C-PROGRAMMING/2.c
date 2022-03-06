91524f23299d319d300422693ac7e45ace3d9be2
SoumyaDhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-06 04:45:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@SOUMYA-C-PROGRAMMING/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+
+
+{
+  int a,b;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  printf("enter a number\n");
+  scanf("%d",&b);
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@SOUMYA-C-PROGRAMMING/NAME.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+  printf("My name is Soumya\n");
+  
+  return 0;
+}
