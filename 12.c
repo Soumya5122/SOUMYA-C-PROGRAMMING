487495e0b1cd5a6d2678571248a128d5dcf6ed72
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-16 04:53:53 +0000

    main

--- 12.c
+++ 12.c
@@ -3,11 +3,11 @@ int main()
 {
  int marks1, marks2,marks3;
   printf("enter the marks1\n");
-  scanf("%d\n");
+  scanf("%d\n",&marks1);
   printf("enter the marks2\n");
-  scanf("%dd\n");
+  scanf("%dd\n",&marks2);
   printf("enter the marks3\n");
-  scanf("%d\n");
+  scanf("%d\n",&marks3);
 
   float n= marks1+marks2+marks3/3;
   if(n>=40)
