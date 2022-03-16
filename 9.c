1afe7c79cda9531206dd382973d8574d46a0f19c
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-16 04:08:28 +0000

    ternary operator

--- 9.c
+++ 9.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int b=6;
+int b=6             
   int c=7;
   int a= c==b?3:7;
   printf("the value of a is %d",a);
   return 0;
 }
+
+
+
+a=c==b? if this condition is true then a will be 3 otherwise 7.
