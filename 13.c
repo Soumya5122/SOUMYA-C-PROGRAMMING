ba8ad571d43479dc5eb52eaad89d9ba73cbe0d43
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-16 05:25:50 +0000

    while loop

---
+++ 13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int i=0;
+  while(i<45)
+    {
+      printf("the value of i is %d\n",i);
+      i +=1;
+      //i++;
+      //i=i+1;
+        
+    }
+  return 0;
+}
