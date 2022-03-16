9164593e0e8b081f58c8f30cd0426c76ea9ac099
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-16 04:27:14 +0000

    switch cases

---
+++ 10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int age=45;
+  switch(age)
+    {
+      case 34:
+    printf("you are 34\n");
+    break;
+
+      case 45:
+      printf("you are 45\n");
+      break;
+
+      default:
+      printf("you are 76\n");
+      break;
+      }
+  return 0;
+}
