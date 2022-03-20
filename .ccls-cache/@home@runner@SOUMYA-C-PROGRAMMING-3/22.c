39f390e4101487b1cee63ba2379a9ba08e23bf1e
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-20 13:29:44 +0000

    main

---
+++ .ccls-cache/@home@runner@SOUMYA-C-PROGRAMMING-3/22.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int factorial(int)
+
+int main()
+{
+  printf("the value of factorial 6 is %d",factorial(6));
+  return 0;
+}
+
+int factorial(int n)
+{
+  if(n==1 || n==0)
+    
+  {
+    return 1;
+    }
+  return n* factorial(n-1);
+}
---
+++ 23.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  float a=8;
+  printf("the square of a is %f\n",pow(a,2));
+  return 0;
+}
