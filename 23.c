072e943f352cab98c4c23d6e033c079b2df9bb14
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-20 16:44:33 +0000

    main

--- 23.c
+++ 23.c
@@ -1,8 +1,31 @@
 #include<stdio.h>
 #include<math.h>
+int input()
+{
+  float a,b,c;
+  printf("enter the three numbers\n");
+  scanf("%f,%f, %f\n",&a,&b,&c);
+  return a,b,c;
+}
+
+int average(int a,int b,int c)
+{
+  int avg;
+  avg=(a+b+c)/3;
+  return avg;
+}
+
+void output(int a,int b,int c,int result)
+{
+  printf("the average of  %f + %f + %f/3 is %f\n",a,b,c,result);
+}
+
 int main()
 {
-  float a=8;
-  printf("the square of a is %f\n",pow(a,2));
+  int a,b,c,result;
+  input(a,b,c);
+  result=average(a,b,c);
+  output(a,b,c,result);
   return 0;
+  
 }
