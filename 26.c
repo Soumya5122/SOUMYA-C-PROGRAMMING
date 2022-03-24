42f99623ca90efc2b61c54a555314eea0baf23f2
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-24 09:21:13 +0000

    main

--- 26.c
+++ 26.c
@@ -1,32 +1,23 @@
 #include<stdio.h>
 
-//sum of natural numbers=1 + 2+ 3+.......+(n-1)+n
- sum=sum(n-1)+n//
+//sum of natural numbers=1 + 2+ 3+.......+(n-1)+n ,sum=sum(n-1)+n
+ //
 
-int input()
-{
-  int n;
-  printf("enter a number");
-  scanf("%d"&n);
-}
-int sum()
-{
-  int sum,n;
-  for(int i=1,i<=5;i++)
-  {
-  sum=sum(n-1)+n;
-    }
-  return sum;
-}
-void output()
-{
-  printf("%d the sum of n natural numbers",sum);
-}
+int sum();
+
 int main()
 {
-  int n,sum;
-  input(n);
-  sum(sum(n-1)+n);
-  output(sum);
+  
+  printf("the sum of 5 n natural numkbers is %d",sum(5));
+  
 }
 
+int sum(int n)
+
+{
+  if(n==1)
+  {
+    return 1;
+  }
+  return sum(n-1)+n;
+}
