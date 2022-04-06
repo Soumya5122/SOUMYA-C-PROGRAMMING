984b8cd2939a84f2e8a24d19470293096b65e268
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-04-06 14:51:48 +0000

    positive / negative

---
+++ positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("enter the value\n");
+  scanf("%d",&a);
+
+  if(a>0)
+  {
+    printf("it is a positive number");
+  }
+  else
+    {
+      printf("it is a negative number");
+    }
+  return 0;
+}
