227a5fb11ba52ab6afd82a181a8405e3ac8116d9
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-04-06 13:53:12 +0000

    even /odd

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("enter a value\n");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+    printf("it is even");
+  }
+  else
+  {
+    printf("it is odd");
+  }
+  return 0;
+}
